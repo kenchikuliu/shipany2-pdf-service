480768317d48a3f58c0bffcf3cc85f8290de3d91
KJANFDHL <44759890+kenchikuliu@users.noreply.github.com>
2026-04-23 19:44:19 +0800

    chore: migrate to chromium-min for vercel compatibility

--- src/app/api/pdf/route.ts
+++ src/app/api/pdf/route.ts
@@ -1,4 +1,4 @@
-import chromium from '@sparticuz/chromium';
+import chromium from '@sparticuz/chromium-min';
 import puppeteer from 'puppeteer-core';
 import { NextRequest, NextResponse } from 'next/server';
 
